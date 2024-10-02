62589dff8e0aadf42d52a18bad905ec7d331e038
c3678161 <m.ali6877@student.leedsbeckett.ac.uk>
2024-10-02 10:23:53 +0100

    task 1 completed.

---
+++ lab 2/Counter.java
@@ -0,0 +1,8 @@
+public interface Counter {
+
+    int countWords(String sentence); //counts the words of the sentences.
+    int countLetters(String sentence); //counts the letters within each sentence.
+    int getLength(String sentence); //counts the sentences.
+
+
+}
